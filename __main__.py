0bc81693b78fcf625da510cc224ec2a7297d452d
vjgarciag96 <vjgarciag96@gmail.com>
2018-02-01 16:38:44 +0100

    implemented all messages deleting

--- __main__.py
+++ __main__.py
@@ -1,7 +1,10 @@
 from telegram.ext import CommandHandler
+from telegram.ext import Filters
+from telegram.ext import MessageHandler
 from telegram.ext import Updater
 from keys import TOKEN
 from commands import shutup
+from messages import drop_message
 
 if __name__ == '__main__':
     updater = Updater(token=TOKEN)
@@ -9,5 +12,8 @@ if __name__ == '__main__':
 
     shut_up_command_handler = CommandHandler('shutup', shutup)
     dispatcher.add_handler(shut_up_command_handler)
+
+    delete_messages_handler = MessageHandler(Filters.text, drop_message)
+    dispatcher.add_handler(delete_messages_handler)
 
     updater.start_polling()
