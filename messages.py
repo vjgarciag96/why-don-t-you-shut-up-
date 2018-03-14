e153c2569431c8f4c5f36d24d0db39025c0f0a52
fdiazgon <fediazgon@gmail.com>
2018-03-14 23:59:54 +0100

    Avoid Pery's spam

--- messages.py
+++ messages.py
@@ -1,10 +1,9 @@
-import urllib.request
 from keys import TOKEN
 
 BASE_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
+
 def drop_message(bot, update):
     chat_id = update.message.chat.id
     message_id = update.message.message_id
-    url = BASE_URL + "deleteMessage?chat_id={}&message_id={}".format(chat_id, message_id)
-    response = urllib.request.urlopen(url).read()
+    bot.delete_message(chat_id, message_id)
